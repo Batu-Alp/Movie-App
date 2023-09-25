13b7ddafc08458700d0197d22c68944b0ff435d8
Batu Alp <batubenlican@gmail.com>
2023-09-25 15:46:58 +0300

    Edited

---
+++ movie-app/src/main/java/com/example/movieapp/Movie.java
@@ -0,0 +1,38 @@
+package com.example.movieapp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imbdId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailer;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
+
+    private List<Review> reviewId;
+
+}
