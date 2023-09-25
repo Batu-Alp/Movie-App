6860d9efcb93dae21c593f0ca831168f0e030924
Batu Alp <batubenlican@gmail.com>
2023-09-25 16:43:51 +0300

    Reviews added

---
+++ movie-app/src/main/java/com/example/movieapp/ReviewService.java
@@ -0,0 +1,33 @@
+package com.example.movieapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    public ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imbdId) {
+
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        // Updating reviewIds arrays
+        // imbdId ye göre match ediliyor
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imbdId").is(imbdId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+
+    }
+
+}
